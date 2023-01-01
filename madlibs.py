1c28a91f2ab8c6f433ea0776bedf376e0861556c
Runi Panda <115415915+Runi-13@users.noreply.github.com>
2023-01-02 00:19:00 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+
+"""
+Objective- to concatenate strings
+Method- By using f-string 
+there is no terminal available so this is a alternative approach:-
+1. whatever written in terminal we can write same thing in input file as well
+2. now we need to read file to extract data from that file 
+"""
+
+
+
+with open(r'C:\Users\urban\OneDrive\Desktop\Python\Project1\input.txt') as f:
+    lines = f.readlines()
+    print(lines)
+
+
+#family=input("family: ")
+#adj=input("adj: m")   
+#introduction = f"musa belongs to belong to {family} is considered to be one of the oldest crops as well as a {adj} " 
+#print(introduction)
+
+
+
+
+
